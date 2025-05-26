8a357abdff4ec17b3afaf55b452fd399f2dfec3a
Lucas Fiduniv <lucasgabriel22222222@gmail.com>
2025-05-26 10:20:07 -0300

    feat: :sparkles: criar módulo principal da aplicação com configuração do NestJS

---
+++ src/app.module.ts
@@ -0,0 +1,67 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BullModule } from '@nestjs/bullmq';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/projects.module';
+import { ProposalsModule } from './proposals/proposals.module';
+import { ContractsModule } from './contracts/contracts.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { MailModule } from './mail/mail.module';
+import { QueueModule } from './queue/queue.module';
+import { CommonModule } from './common/common.module';
+import { configuration } from './config/configuration';
+
+@Module({
+  imports: [
+    // Configuration
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+
+    // Database
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: configService.get('database.synchronize'),
+        ssl: configService.get('database.ssl'),
+      }),
+    }),
+
+    // Redis and BullMQ
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        connection: {
+          host: configService.get<string>('redis.host'),
+          port: configService.get<number>('redis.port'),
+          password: configService.get<string>('redis.password'),
+        },
+      }),
+
+    }),
+
+    // Feature modules
+    AuthModule,
+    UsersModule,
+    ProjectsModule,
+    ProposalsModule,
+    ContractsModule,
+    ReviewsModule,
+    MailModule,
+    QueueModule,
+    CommonModule,
+  ],
+})
+export class AppModule { }
