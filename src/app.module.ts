907b22194e254231055708d52fe7c31fa1efcddf
Lucas Fiduniv <lucasgabriel22222222@gmail.com>
2025-05-29 23:26:58 -0300

    feat: :sparkles: refatorar a configuração do TypeORM e BullMQ para utilizar arquivos de configuração dedicados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BullModule } from '@nestjs/bullmq';
+
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
@@ -11,7 +12,10 @@ import { ReviewsModule } from './reviews/reviews.module';
 import { MailModule } from './mail/mail.module';
 import { QueueModule } from './queue/queue.module';
 import { CommonModule } from './common/common.module';
+
 import { configuration } from './config/configuration';
+import { typeOrmConfig } from './config/typeorm.config';
+import { bullMqConfig } from './config/bullmq.config';
 
 @Module({
   imports: [
@@ -21,57 +25,8 @@ import { configuration } from './config/configuration';
       load: [configuration],
     }),
 
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        const dbHost = configService.get<string>('DB_HOST', 'localhost');
-        const dbPort = configService.get<number>('DB_PORT', 5435);
-        const dbUsername = configService.get<string>('DB_USERNAME', 'postgres');
-        const dbPassword = configService.get<string>('DB_PASSWORD', 'postgres');
-        const dbName = configService.get<string>('DB_NAME', 'conecta_freela');
-        const dbSsl = configService.get<string>('DB_SSL') === 'true';
-
-        console.log('--- DATABASE CONFIG ---');
-        console.log('DB_SSL:', dbSsl);
-
-        return {
-          type: 'postgres',
-          host: dbHost,
-          port: dbPort,
-          username: dbUsername,
-          password: dbPassword,
-          database: dbName,
-          ssl: dbSsl ? { rejectUnauthorized: false } : false,
-          synchronize: true,
-          autoLoadEntities: true,
-          logging: true,
-        };
-      }
-    }),
-
-    BullModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        const redisHost = configService.get<string>('REDIS_HOST', 'localhost');
-        const redisPort = configService.get<number>('REDIS_PORT', 6379);
-        const redisPassword = configService.get<string>('REDIS_PASSWORD', '');
-
-        console.log('--- REDIS CONFIG ---');
-        console.log('REDIS_HOST:', redisHost);
-        console.log('REDIS_PORT:', redisPort);
-        console.log('REDIS_PASSWORD:', redisPassword);
-
-        return {
-          connection: {
-            host: redisHost,
-            port: redisPort,
-            password: redisPassword || undefined,
-          },
-        };
-      },
-    }),
+    TypeOrmModule.forRootAsync(typeOrmConfig),
+    BullModule.forRootAsync(bullMqConfig),
 
     AuthModule,
     UsersModule,
