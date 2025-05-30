83fdfcc06fb918dd35eb6c600de21e03a59734c0
Lucas Fiduniv <lucasgabriel22222222@gmail.com>
2025-05-30 00:02:53 -0300

    feat: :sparkles: implementar caso de uso FindReviewById para buscar uma avaliação pelo ID

---
+++ src/reviews/usecases/find-review-by-id.usecase.ts
@@ -0,0 +1,21 @@
+import { Injectable, Inject, NotFoundException } from '@nestjs/common';
+import { Review } from '../entities/review.entity';
+import { IReviewRepository } from '../ReviewsRepository/IReviewRepository';
+
+@Injectable()
+export class FindReviewByIdUseCase {
+    constructor(
+        @Inject('IReviewRepository')
+        private readonly reviewRepository: IReviewRepository,
+    ) { }
+
+    async execute(id: string): Promise<Review> {
+        const review = await this.reviewRepository.findOneById(id);
+
+        if (!review) {
+            throw new NotFoundException(`Review with ID ${id} not found`);
+        }
+
+        return review;
+    }
+}
