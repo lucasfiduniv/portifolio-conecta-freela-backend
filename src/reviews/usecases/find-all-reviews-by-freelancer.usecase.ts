5c22446a7f61d169b7a3a486ed286c8c8e0781f2
Lucas Fiduniv <lucasgabriel22222222@gmail.com>
2025-05-30 00:02:44 -0300

    feat: :sparkles: implementar caso de uso FindAllReviewsByFreelancer para buscar avaliações de freelancers

---
+++ src/reviews/usecases/find-all-reviews-by-freelancer.usecase.ts
@@ -0,0 +1,16 @@
+import { Injectable, Inject } from '@nestjs/common';
+
+import { Review } from '../entities/review.entity';
+import { IReviewRepository } from '../ReviewsRepository/IReviewRepository';
+
+@Injectable()
+export class FindAllReviewsByFreelancerUseCase {
+    constructor(
+        @Inject('IReviewRepository')
+        private readonly reviewRepository: IReviewRepository,
+    ) { }
+
+    async execute(freelancerId: string): Promise<Review[]> {
+        return this.reviewRepository.findAllByFreelancer(freelancerId);
+    }
+}
