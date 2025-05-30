91051bd2929dcb0cd29820cca72f88618d01bd33
Lucas Fiduniv <lucasgabriel22222222@gmail.com>
2025-05-30 00:03:21 -0300

    refactor: :recycle: reorganizar importações e formatação no módulo de usuários

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
+import { UsersController } from './users.controller';
 import { CreateUserUseCase } from './usecases/create-user.usecase';
 import { FindAllUsersUseCase } from './usecases/find-all-users.usecase';
 import { FindOneUserUseCase } from './usecases/find-one-user.usecase';
@@ -9,14 +9,18 @@ import { FindUserByEmailUseCase } from './usecases/find-user-by-email.usecase';
 import { AddRoleUseCase } from './usecases/add-role.usecase';
 import { UpdateRatingUseCase } from './usecases/update-rating.usecase';
 import { UsersService } from './users.service';
+import { UserRepository } from './infra/UserRepository'; // caminho correto da implementação
+import { IUserRepository } from './UserRepository/IUserRepository'; // apenas para tipo, não obrigatório aqui
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([User]),
-  ],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [
     UsersService,
+    {
+      provide: 'IUserRepository',
+      useClass: UserRepository,
+    },
     CreateUserUseCase,
     FindAllUsersUseCase,
     FindOneUserUseCase,
