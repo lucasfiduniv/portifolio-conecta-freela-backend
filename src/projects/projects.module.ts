3f2adc91df70c1bf1005c9af456af32a745f4ccf
Lucas Fiduniv <lucasgabriel22222222@gmail.com>
2025-06-03 23:26:25 -0300

    refactor: :recycle: reorganizar imports e providers no módulo de projetos

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,15 +1,26 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProjectsController } from './projects.controller';
-import { ProjectsService } from './projects.service';
 import { Project } from './entities/project.entity';
+import { ProjectsService } from './projects.service';
+import { ProjectRepository } from './infra/ProjectRepository';
+import { CreateProjectUseCase } from './usecases/create-project/create-project.usecase';
+import { FindOneProjectUseCase } from './usecases/find-one-project/find-one-project.usecase';
+import { RemoveProjectUseCase } from './usecases/remove-project/remove-project.usecase';
+import { FindAllProjectsUseCase } from './usecases/find-all-projects/find-all-projects.usecase';
+
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Project]),
+  imports: [TypeOrmModule.forFeature([Project])],
+  providers: [
+    ProjectsService,
+    ProjectRepository,
+    { provide: 'IProjectRepository', useExisting: ProjectRepository },
+
+    CreateProjectUseCase,
+    FindAllProjectsUseCase,
+    FindOneProjectUseCase,
+    RemoveProjectUseCase,
   ],
-  controllers: [ProjectsController],
-  providers: [ProjectsService],
   exports: [ProjectsService],
 })
-export class ProjectsModule {}
+export class ProjectsModule { }
